ed3094c30311d0677377f5fc9b77532e7ee7c6de
GmoLargey <ocgovr2018@gmail.com>
2026-05-21 23:36:08 +0100

    Async and async cache for compatible dxvk versions default to enabled in container.java
    Qualcomm_V849 driver selected as wrapper for quest 3 devices (quest 3 ''system'' has broken 2.6.2-1 compatibility)
    
    DXVK version defaults to 2.6.2-1 for Quest 3 devices ( fixes rendering issues on qualcomm driver )

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -1,11 +1,22 @@
 package com.winlator.cmod.core;
 
+import static com.winlator.xr.Device.HmdModel.PICO_UNKNOWN;
+import static com.winlator.xr.Device.HmdModel.QUEST_2;
+
+import com.winlator.xr.Device;
+
 public abstract class DefaultVersion {
     public static final String BOX86 = "0.4.2-0";
     public static final String BOX64 = "0.4.2-0";
     public static final String FEXCORE = "2605-0";
-    public static final String WRAPPER = "System";
-    public static final String DXVK = "1.10.1";
+    public static final String WRAPPER = switch(Device.getDevice()) {
+        case QUEST_3 -> "Qualcomm_v849_Quest3_Pico4Ultra";
+        default -> "System";
+    };
+    public static final String DXVK = switch(Device.getDevice()) {
+        case QUEST_3 -> "2.6.2-1-gplasync-1";
+        default -> "1.10.1";
+    };
     public static final String D8VK = "1.0";
     public static final String VKD3D = "2.12-0";
 }
