d914e02c95e3a21a87ea6f92cbb3d24a31470f18
GmoLargey <ocgovr2018@gmail.com>
2026-05-22 23:58:53 +0100

    pico hmds detect and set best defaults

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -11,10 +11,14 @@ public abstract class DefaultVersion {
     public static final String FEXCORE = "2605-0";
     public static final String WRAPPER = switch(Device.getDevice()) {
         case QUEST_3 -> "Qualcomm_v849_Quest3_Pico4Ultra";
+        case PICO_4_Ultra -> "adrenotools-Turnip_V26.2.0_R4";
         default -> "System";
     };
     public static final String DXVK = switch(Device.getDevice()) {
         case QUEST_3 -> "2.6.2-1-gplasync-1";
+        case PICO_4_Ultra -> "2.6.2-1-gplasync-1";
+        case PICO_NEO_3_LINK -> "1.12.-sarek-async-0";
+        case PICO_4 -> "1.12.-sarek-async-0";
         default -> "1.10.1";
     };
     public static final String D8VK = "1.0";
--- app/src/main/java/com/winlator/xr/Device.java
+++ app/src/main/java/com/winlator/xr/Device.java
@@ -1,24 +1,7 @@
-/*
- * Copyright (C) 2024-2026 WinlatorXR
- *
- * This file is part of WinlatorXR.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package com.winlator.xr;
 
 import android.os.Build;
+import android.util.Log;
 
 import com.winlator.xr.runtime.MetaQuest;
 import com.winlator.xr.runtime.Pico;
@@ -27,15 +10,17 @@ import com.winlator.xr.runtime.PlayForDream;
 public class Device {
 
     public enum HmdModel {
-        PICO_UNKNOWN,
+        PICO_UNKNOWN, PICO_4_Ultra, PICO_NEO_3_LINK, PICO_4,
         PLAY_FOR_DREAM_UNKNOWN,
         QUEST_1, QUEST_2, QUEST_3, QUEST_PRO, QUEST_UNKNOWN,
         UNKNOWN_DEVICE
     }
 
     public static HmdModel getDevice() {
+        Log.e("ABCDE", "MANUFACTURER: " + Build.MANUFACTURER);
+        Log.e("ABCDE", "PRODUCT: " + Build.PRODUCT);
         if (Build.MANUFACTURER.compareToIgnoreCase("PICO") == 0) {
-            return HmdModel.PICO_UNKNOWN;
+            return getPicoDevice();
         } else if (Build.MANUFACTURER.compareToIgnoreCase("PLAY FOR DREAM") == 0) {
             return HmdModel.PLAY_FOR_DREAM_UNKNOWN;
         } else if (Build.MANUFACTURER.compareToIgnoreCase("OCULUS") == 0) {
@@ -68,6 +53,14 @@ public class Device {
             case "eureka", "stinson", "panther" -> Device.HmdModel.QUEST_3;
             case "seacliff" -> Device.HmdModel.QUEST_PRO;
             default -> Device.HmdModel.QUEST_UNKNOWN;
+        };
+    }
+    private static Device.HmdModel getPicoDevice() {
+        return switch (Build.PRODUCT) {
+            case "PICO 4 Ultra", "Pico_A9210", "A9210", "sparrow" -> Device.HmdModel.PICO_4_Ultra;
+            case "Pico Neo 3", "Pico_Neo_3", "A7P10" -> HmdModel.PICO_NEO_3_LINK;
+            case "Pico 4", "Pico_4", "PICO 4", "PICO_4", "Pico A8110", "PICO A8110", "Pico_A8110", "PICOA8110", "A8110", "pheonix" -> HmdModel.PICO_4;
+            default -> Device.HmdModel.PICO_UNKNOWN;
         };
     }
 }
