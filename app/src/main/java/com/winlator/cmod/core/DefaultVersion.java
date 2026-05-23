3d11ac4a66730ed2c810a0f939ad1a68f87608d0
Lubos <tridosm@gmail.com>
2026-05-23 11:58:47 +0200

    Lint fixed

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -1,8 +1,5 @@
 package com.winlator.cmod.core;
 
-import static com.winlator.xr.Device.HmdModel.PICO_UNKNOWN;
-import static com.winlator.xr.Device.HmdModel.QUEST_2;
-
 import com.winlator.xr.Device;
 
 public abstract class DefaultVersion {
@@ -10,15 +7,13 @@ public abstract class DefaultVersion {
     public static final String BOX64 = "0.4.2-0";
     public static final String FEXCORE = "2605-0";
     public static final String WRAPPER = switch(Device.getDevice()) {
+        case PICO_4_ULTRA -> "adrenotools-Turnip_V26.2.0_R4";
         case QUEST_3 -> "Qualcomm_v849_Quest3_Pico4Ultra";
-        case PICO_4_Ultra -> "adrenotools-Turnip_V26.2.0_R4";
         default -> "System";
     };
     public static final String DXVK = switch(Device.getDevice()) {
-        case QUEST_3 -> "2.6.2-1-gplasync-1";
-        case PICO_4_Ultra -> "2.6.2-1-gplasync-1";
-        case PICO_NEO_3_LINK -> "1.12.-sarek-async-0";
-        case PICO_4 -> "1.12.-sarek-async-0";
+        case PICO_NEO_3_LINK, PICO_4 -> "1.12.-sarek-async-0";
+        case PICO_4_ULTRA, QUEST_3 -> "2.6.2-1-gplasync-1";
         default -> "1.10.1";
     };
     public static final String D8VK = "1.0";
--- app/src/main/java/com/winlator/xr/Device.java
+++ app/src/main/java/com/winlator/xr/Device.java
@@ -19,7 +19,6 @@
 package com.winlator.xr;
 
 import android.os.Build;
-import android.util.Log;
 
 import com.winlator.xr.runtime.MetaQuest;
 import com.winlator.xr.runtime.Pico;
@@ -28,15 +27,13 @@ import com.winlator.xr.runtime.PlayForDream;
 public class Device {
 
     public enum HmdModel {
-        PICO_UNKNOWN, PICO_4_Ultra, PICO_NEO_3_LINK, PICO_4,
+        PICO_NEO_3_LINK, PICO_4, PICO_4_ULTRA, PICO_UNKNOWN,
         PLAY_FOR_DREAM_UNKNOWN,
         QUEST_1, QUEST_2, QUEST_3, QUEST_PRO, QUEST_UNKNOWN,
         UNKNOWN_DEVICE
     }
 
     public static HmdModel getDevice() {
-        Log.e("ABCDE", "MANUFACTURER: " + Build.MANUFACTURER);
-        Log.e("ABCDE", "PRODUCT: " + Build.PRODUCT);
         if (Build.MANUFACTURER.compareToIgnoreCase("PICO") == 0) {
             return getPicoDevice();
         } else if (Build.MANUFACTURER.compareToIgnoreCase("PLAY FOR DREAM") == 0) {
@@ -64,6 +61,15 @@ public class Device {
         }
     }
 
+    private static Device.HmdModel getPicoDevice() {
+        return switch (Build.PRODUCT) {
+            case "Pico Neo 3", "Pico_Neo_3", "A7P10" -> HmdModel.PICO_NEO_3_LINK;
+            case "Pico 4", "Pico_4", "PICO 4", "PICO_4", "Pico A8110", "PICO A8110", "Pico_A8110", "PICOA8110", "A8110", "pheonix" -> HmdModel.PICO_4;
+            case "PICO 4 Ultra", "Pico_A9210", "A9210", "sparrow" -> Device.HmdModel.PICO_4_ULTRA;
+            default -> Device.HmdModel.PICO_UNKNOWN;
+        };
+    }
+
     private static Device.HmdModel getQuestDevice() {
         return switch (Build.PRODUCT) {
             case "monterey", "vr_monterey" -> Device.HmdModel.QUEST_1;
@@ -71,14 +77,6 @@ public class Device {
             case "eureka", "stinson", "panther" -> Device.HmdModel.QUEST_3;
             case "seacliff" -> Device.HmdModel.QUEST_PRO;
             default -> Device.HmdModel.QUEST_UNKNOWN;
-        };
-    }
-    private static Device.HmdModel getPicoDevice() {
-        return switch (Build.PRODUCT) {
-            case "PICO 4 Ultra", "Pico_A9210", "A9210", "sparrow" -> Device.HmdModel.PICO_4_Ultra;
-            case "Pico Neo 3", "Pico_Neo_3", "A7P10" -> HmdModel.PICO_NEO_3_LINK;
-            case "Pico 4", "Pico_4", "PICO 4", "PICO_4", "Pico A8110", "PICO A8110", "Pico_A8110", "PICOA8110", "A8110", "pheonix" -> HmdModel.PICO_4;
-            default -> Device.HmdModel.PICO_UNKNOWN;
         };
     }
 }
