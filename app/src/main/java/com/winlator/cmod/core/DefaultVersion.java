c618640cca3264892f90a75d3167b7e55e099893
Lubos V. <tridosm@gmail.com>
2026-05-31 01:48:47 +0200

    Fix box version issue(s)

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -3,8 +3,8 @@ package com.winlator.cmod.core;
 import com.winlator.xr.Device;
 
 public abstract class DefaultVersion {
-    public static final String BOX86 = "0.4.2-0";
-    public static final String BOX64 = "0.4.2-0";
+    public static final String BOX86 = "0.4.2";
+    public static final String BOX64 = "0.4.2";
     public static final String FEXCORE = "2605-0";
     public static final String WRAPPER = switch(Device.getDevice()) {
         case PICO_4_ULTRA -> "adrenotools-Turnip_V26.2.0_R4";
