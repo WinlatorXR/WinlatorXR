ba8ae1f81ac83a24fbe80aff6cc69ca3e96d1767
GmoLargey <ocgovr2018@gmail.com>
2026-06-14 07:50:27 +0100

    Use 819.2 as quest 3 new default wrapper, as this is the last working meta qualcomm driver to present a gpu to FEX based containers.

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -8,7 +8,7 @@ public abstract class DefaultVersion {
     public static final String FEXCORE = "2605-0";
     public static final String WRAPPER = switch(Device.getDevice()) {
         case PICO_4_ULTRA -> "adrenotools-Turnip_v26.2.0_R6";
-        case QUEST_3 -> "adrenotools-v849_Quest3_Pico4Ultra";
+        case QUEST_3 -> "adrenotools-v819.2_Quest3_Pico4Ultra";
         case QUEST_2 -> "adrenotools-Turnip_v26.2.0_R6";
         default -> "System";
     };
