927be307b84d937674717c4986c199885eca8b7a
GmoLargey <ocgovr2018@gmail.com>
2026-06-20 01:11:43 +0100

    overhaul of included FEX, DXVK and VKD3D, all local and not from contents, this allows logic of x86/arm to show compatible dxvk and vkd3d only.

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -5,7 +5,7 @@ import com.winlator.xr.Device;
 public abstract class DefaultVersion {
     public static final String BOX86 = "0.4.2";
     public static final String BOX64 = "0.4.2";
-    public static final String FEXCORE = "2605-0";
+    public static final String FEXCORE = "2605";
     public static final String WRAPPER = switch(Device.getDevice()) {
         case PICO_4_ULTRA, QUEST_2 -> "adrenotools-Turnip_v26.2.0_R6";
         case QUEST_3 -> "adrenotools-v819.2_Quest3_Pico4Ultra";
@@ -15,6 +15,6 @@ public abstract class DefaultVersion {
         case PICO_NEO_3_LINK, PICO_4 -> "1.12.-sarek-async-0";
         default -> "1.10.1";
     };
-    public static final String D8VK = "1.0";
+    public static final String D8VK = "1.10.1";
     public static final String VKD3D = "2.12-0";
 }
